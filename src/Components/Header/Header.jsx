08a2c8fe0c7157409983983be85598eda15af013
chathura226 <chathuralakshan226@gmail.com>
2024-05-31 20:30:24 +0530

    basic sturecture and router starter code

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './Header.css'
+import NavBar from './NavBar'
+
+function Header() {
+    return (
+
+        <div id="navContent">
+            <NavBar />
+        </div>
+    )
+}
+
+export default Header
