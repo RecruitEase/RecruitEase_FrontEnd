cb52c18ee0c526cac9a274a6528e230eede22cd0
SanduniIshara <sanduniishara1@outlook.com>
2024-06-14 10:43:31 +0530

    JobView part partially completed

---
+++ src/Components/JobViewComponents/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Hero.css'
+
+const Hero = () => {
+  return (
+    <div className='hero container'>
+     <div className='hero-text'>
+       <h1>Front-End Developper</h1>
+       <p>IFS Colombo</p>
+       
+     </div> 
+    </div>
+  )
+}
+
+export default Hero
