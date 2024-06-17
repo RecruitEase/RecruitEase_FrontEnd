d7693350081845c2bb4be7ef456e41c8f1e31b59
D.N.S. BANDARA <136679322+Sajith091@users.noreply.github.com>
2024-06-17 08:13:26 +0530

    remove draft option

--- src/Components/Profile/StatusOfApplications.jsx
+++ src/Components/Profile/StatusOfApplications.jsx
@@ -4,12 +4,12 @@ const StatusOfApplications = () => {
     return ( 
         <div className="staus-main">
             <div>
-                <h2>Status of Aplications</h2>
-                <p>0 Aplycations</p>
+                <h2>Status of Applications</h2>
+                <p>0 Applications</p>
             </div>
             <br/>
             <div className="empty">
-                <div className="empty-comtent"><p>No Aplications Founds!</p></div>
+                <div className="empty-comtent"><p>No Applications Founds!</p></div>
             </div>
 
         </div>
