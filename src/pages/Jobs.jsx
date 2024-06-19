c8d4146dec973d6bcb52b9a599c050caa7403e83
Madasha Liyanage <2021is055@stu.ucsc.cmb.ac.lk>
2024-06-19 16:54:44 +0530

    modified:   src/Components/Candidate/Navbar.jsx 	modified:   src/pages/Jobs.jsx

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,11 +1,79 @@
-import react from 'react';
+import * as React from 'react';
+import { styled, createTheme, ThemeProvider } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Grid';
+import FormControl from '@mui/material/FormControl';
+import InputLabel from '@mui/material/InputLabel';
+import Select from '@mui/material/Select';
+import MenuItem from '@mui/material/MenuItem';
 
-const Jobs = () => {
-    return (
-        <div>
-        <h1>Jobs</h1>
-        </div>
-    );
-    }
+const Item = styled(Paper)(({ theme }) => ({
+  backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
-export default Jobs;
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: 'rgb(244, 244, 244)',
+      dark: '#0066CC',
+    },
+  },
+  typography: {
+    body2: {
+      fontSize: 14,
+    },
+  },
+});
+
+export default function Jobs() {
+  const [age, setAge] = React.useState('');
+
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
+
+  return (
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <Grid container spacing={2} columns={5}>
+          <Grid item xs={2}>
+            <Box
+              sx={{
+                width: '100%',
+                height: 100,
+                borderRadius: 1,
+                bgcolor: 'primary.main',
+               
+              }}
+            >
+              <Box sx={{ minWidth: 120 }}>
+                <FormControl fullWidth>
+                  <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                  <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Age"
+                    onChange={handleChange}
+                  >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                  </Select>
+                </FormControl>
+              </Box>
+            </Box>
+          </Grid>
+          <Grid item xs={3}>
+            <Item>xs=3</Item>
+          </Grid>
+        </Grid>
+      </Box>
+    </ThemeProvider>
+  );
+}
