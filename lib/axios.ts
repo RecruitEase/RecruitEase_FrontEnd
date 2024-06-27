a1ef8c87069b0633773bc9a3543f1cf60725a9b2
chathura226 <chathuralakshan226@gmail.com>
2024-06-27 14:13:57 +0530

    configured axios for automatic header with atuhroization token

--- lib/axios.ts
+++ lib/axios.ts
@@ -1,1 +1,18 @@
-const BASE_URL = process.env.API_GATEWAY_URL;
+import axios from "axios";
+
+const BASE_URL = process.env.API_GATEWAY_BASE_URL;
+
+export default axios.create({
+    baseURL: "http://localhost:8222",
+    headers: {
+        "Content-type": "application/json"
+    }
+})
+
+//to work with interceptors
+export const axiosWithAuth=axios.create({
+    baseURL: "http://localhost:8222",
+    headers: {
+        "Content-type": "application/json"
+    }
+})
