6c17655a3d371eaf4ca76fa049893367345d34b1
Sajith-Bandara <sajithbandara091@gmail.com>
2024-07-01 22:33:21 +0530

    edited profile details view

--- components/candidateProfile/personalDetails.tsx
+++ components/candidateProfile/personalDetails.tsx
@@ -5,6 +5,8 @@ import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button } 
 import { Avatar } from "@nextui-org/react";
 import {Chip} from "@nextui-org/react";
 import Carousel from "@/components/carousel";
+import {ScrollShadow} from "@nextui-org/react";
+
 
 const PersonalDetails = () => {
     const [isExpanded, setIsExpanded] = useState(false);
@@ -22,30 +24,21 @@ const PersonalDetails = () => {
                         <p className="text-md font-bold">About me</p>
                     </CardHeader>
                     <Divider/>
-                    <CardBody className={isExpanded ? "" : "max-h-[153px] overflow-hidden"}>
-                        <p>
-                            I'm a Product Designer based in Dallas, Texas. I specialize in UX/UI design, product design, and
-                            code development. I'm always striving to grow and learn something new, trying to do stuff that is
-                            outside my comfort zone.
-                            My work has been featured on Dribble, Behance, Webflow, Awwwards, CSS Winner, Mobbin,
-                            and Editor X.
-                            I'm a Product Designer based in Dallas, Texas. I specialize in UX/UI design, product design, and
-                            code development. I'm always striving to grow and learn something new, trying to do stuff that is
-                            outside my comfort zone.
-                            My work has been featured on Dribble, Behance, Webflow, Awwwards, CSS Winner, Mobbin,
-                            and Editor X.
-                        </p>
-                    </CardBody>
+
+                    <ScrollShadow className="w-full h-[120px] pl-4">
+                        I'm a Product Designer based in Dallas, Texas. I specialize in UX/UI design, product design, and
+                                code development. I'm always striving to grow and learn something new, trying to do stuff that is
+                                outside my comfort zone.
+                                My work has been featured on Dribble, Behance, Webflow, Awwwards, CSS Winner, Mobbin,
+                                and Editor X.
+                                I'm a Product Designer based in Dallas, Texas. I specialize in UX/UI design, product design, and
+                                code development. I'm always striving to grow and learn something new, trying to do stuff that is
+                                outside my comfort zone.
+                                My work has been featured on Dribble, Behance, Webflow, Awwwards, CSS Winner, Mobbin,
+                              and Editor X.
+                    </ScrollShadow>
                     <Divider/>
 
-
-
-                    <CardFooter className={"p-2"}>
-                        <Button variant="bordered"  onClick={toggleExpand}>
-                            {isExpanded ? "Show Less" : "Show More"}
-                        </Button>
-                    </CardFooter>
-                    <Divider/>
                 </Card>
                 <Card className="min-w-[400px] flex-1 mt-4">
                     <CardHeader >
@@ -53,16 +46,32 @@ const PersonalDetails = () => {
                     </CardHeader>
                     <Divider/>
                     <CardBody>
-                        <div className="gap-2 grid grid-cols-2 sm:grid-cols-4">
-                            <Chip size="lg" className={"w-2"}>UX Design</Chip>
-                            <Chip size="lg" className={"max-w-2"}>Product Design</Chip>
-                            <Chip size="lg">Figma</Chip>
-                            <Chip size="lg">Web flow</Chip>
-                            <Chip size="lg">Web flow</Chip>
-                            <Chip size="lg">Web flow</Chip>
-                            <Chip size="lg">Web flow</Chip>
-                            <Chip size="lg">Web flow</Chip>
-                        </div>
+                        <ScrollShadow className="w-full h-[90px] pl-4">
+                            <div className="flex flex-row flex-wrap gap-2">
+                                <Chip size="lg" className={"w-2"}>UX Design</Chip>
+                                <Chip size="lg" className={"max-w-2"}>Product Design</Chip>
+                                <Chip size="lg">Figma</Chip>
+                                <Chip size="lg">Web flow</Chip>
+                                <Chip size="lg">Web flow</Chip>
+                                <Chip size="lg">Web flow</Chip>
+                                <Chip size="lg">Web flow</Chip>
+                                <Chip size="lg">Web flow</Chip>
+
+                            </div>
+                        </ScrollShadow>
+                    </CardBody>
+                    <Divider/>
+
+                    <CardHeader >
+                        <p className="text-md font-bold">Experience</p>
+                    </CardHeader>
+                    <Divider/>
+                    <CardBody>
+                        <ScrollShadow className="w-full h-[138px] pl-4">
+                            <p>I specialise in UX/ Ul design, product design and no-code development.</p>
+                            <p>I specialise in UX/ Ul design, product design and no-code development.</p>
+
+                        </ScrollShadow>
                     </CardBody>
                     <Divider/>
 
@@ -71,11 +80,15 @@ const PersonalDetails = () => {
                     </CardHeader>
                     <Divider/>
                     <CardBody>
-                        <p>I specialise in UX/ Ul design, product design and no-code development.</p>
-                        <p>I specialise in UX/ Ul design, product design and no-code development.</p>
+                        <ScrollShadow className="w-full h-[100px] pl-4">
+                            <p>I specialise in UX/ Ul design, product design and no-code development.</p>
+                            <p>I specialise in UX/ Ul design, product design and no-code development.</p>
+                            <p>I specialise in UX/ Ul design, product design and no-code development.</p>
+                            <p>I specialise in UX/ Ul design, product design and no-code development.</p>
+                            <p>I specialise in UX/ Ul design, product design and no-code development.</p>
+                        </ScrollShadow>
                     </CardBody>
                     <Divider/>
-
 
 
                 </Card>
@@ -99,17 +112,32 @@ const PersonalDetails = () => {
                         <p>sajithbandara@gmail.com</p>
                     </CardBody>
                     <Divider/>
+
                     <CardBody>
-                        <div className="flex gap-4 items-center">
+                        <div className="flex gap-4 items-center ">
                             <Avatar isBordered radius="full" src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSVsHgTWMCMNXjzm54tNMtwsXMQwe0Hej-t0arbxvcXtQxbh7hlxRdmJtOvFg&s" />
                             <Avatar isBordered radius="full" src="https://cdn-icons-png.freepik.com/256/3955/3955056.png" />
                             <Avatar isBordered radius="full" src="https://www.vectorlogo.zone/logos/github/github-tile.svg" />
                             <Avatar isBordered radius="full" src="https://png.pngtree.com/png-vector/20190319/ourmid/pngtree-vector-web-icon-png-image_848026.jpg" />
                         </div>
 
-                        <Carousel/>
+
+                    </CardBody>
+
+                    <Divider/>
+                </Card>
+                <Card className="min-w-[400px] flex-1 mt-4">
+                    <CardHeader className="flex gap-3">
+                        <p className="text-md font-bold">My CV</p>
+                    </CardHeader>
+                    <Divider/>
+                    <CardBody>
+                        <div >
+                            <Carousel/>
+                        </div>
                     </CardBody>
                     <Divider/>
+
                 </Card>
             </div>
         </div>
