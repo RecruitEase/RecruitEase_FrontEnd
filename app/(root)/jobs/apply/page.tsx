ce62046de65a723d5cd9cf825602318418ddcd99
AkilaDemotte <akilamilindademotte@gmail.com>
2024-07-02 14:19:27 +0530

    changes on right side of the pahe

--- app/(root)/jobs/apply/page.tsx
+++ app/(root)/jobs/apply/page.tsx
@@ -41,7 +41,7 @@ const Home = () => {
                     
                     
                    <div className='pt-4 flex justify-center gap-4'>
-                   <Button className=' bg-lime-300 '>
+                   <Button className=' bg-recruitBlue'>
                         Apply For Job
                    </Button>
                    <p className='pt-2 text-xs'>Save draft</p>
@@ -63,8 +63,12 @@ const Home = () => {
 
                         <Card className="col-span-12 sm:col-span-4 h-[600px]">
                         <CardHeader className="absolute z-10 top-1 flex-col !items-start">
-                            <p className="text-tiny  uppercase font-bold">What to watch</p>
-                            <h4 className="font-medium text-large">Stream the Acme event</h4>
+                            <span className=" font-medium">Sales & Marketing Assistant</span>
+                            <p className='text-xs'>Gloria Travels and Tours Pvt Ltd</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" width='50' height='50' viewBox="0 0 496 512"><path d="M248 8C111 8 0 119 0 256S111 504 248 504 496 393 496 256 385 8 248 8zM363 176.7c-3.7 39.2-19.9 134.4-28.1 178.3-3.5 18.6-10.3 24.8-16.9 25.4-14.4 1.3-25.3-9.5-39.3-18.7-21.8-14.3-34.2-23.2-55.3-37.2-24.5-16.1-8.6-25 5.3-39.5 3.7-3.8 67.1-61.5 68.3-66.7 .2-.7 .3-3.1-1.2-4.4s-3.6-.8-5.1-.5q-3.3 .7-104.6 69.1-14.8 10.2-26.9 9.9c-8.9-.2-25.9-5-38.6-9.1-15.5-5-27.9-7.7-26.8-16.3q.8-6.7 18.5-13.7 108.4-47.2 144.6-62.3c68.9-28.6 83.2-33.6 92.5-33.8 2.1 0 6.6 .5 9.6 2.9a10.5 10.5 0 0 1 3.5 6.7A43.8 43.8 0 0 1 363 176.7z"/></svg>
+
+                            <Divider className='w-full' />
+
                         </CardHeader>
                         
                         </Card>
