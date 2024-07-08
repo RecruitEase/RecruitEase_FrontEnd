e1510079d635bc8f6bd44a1178be7831e52d94b9
chathura226 <chathuralakshan226@gmail.com>
2024-07-08 21:42:27 +0530

    finished job post ui with validations

--- app/(newDashoard)/recruiter/post/page.tsx
+++ app/(newDashoard)/recruiter/post/page.tsx
@@ -6,102 +6,129 @@ import {useSession} from "next-auth/react";
 import {Input, Textarea} from "@nextui-org/input";
 import {useForm} from "react-hook-form";
 import CustomInput from "@/components/form_inputs/CustomInput";
-import {Select, SelectItem} from "@nextui-org/react";
+import {DatePicker, Select, SelectItem} from "@nextui-org/react";
 import {toTitleCase} from "@/utils/stringUtils";
 import CustomTextArea from "@/components/form_inputs/CustomTextArea";
+import "@blocknote/core/fonts/inter.css";
+import {useCreateBlockNote} from "@blocknote/react";
+import {BlockNoteView} from "@blocknote/mantine";
+import "@blocknote/mantine/style.css";
+import {Button} from "@nextui-org/button";
 
 const JobPost = () => {
+    const {theme, setTheme} = useTheme();
+    // Creates a new editor instance.
+    const editor = useCreateBlockNote();
+
     const locations = [
-        { key: "work from home", label: "Work From Home" },
-        { key: "island wide", label: "Island Wide" },
-        { key: "ampara", label: "Ampara" },
-        { key: "anuradhapura", label: "Anuradhapura" },
-        { key: "badulla", label: "Badulla" },
-        { key: "batticaloa", label: "Batticaloa" },
-        { key: "colombo", label: "Colombo" },
-        { key: "galle", label: "Galle" },
-        { key: "gampaha", label: "Gampaha" },
-        { key: "hambantota", label: "Hambantota" },
-        { key: "jaffna", label: "Jaffna" },
-        { key: "kalutara", label: "Kalutara" },
-        { key: "kandy", label: "Kandy" },
-        { key: "kegalle", label: "Kegalle" },
-        { key: "kilinochchi", label: "Kilinochchi" },
-        { key: "kurunegala", label: "Kurunegala" },
-        { key: "mannar", label: "Mannar" },
-        { key: "matale", label: "Matale" },
-        { key: "matara", label: "Matara" },
-        { key: "monaragala", label: "Monaragala" },
-        { key: "mullativu", label: "Mullativu" },
-        { key: "nuwara eliya", label: "Nuwara Eliya" },
-        { key: "polonnaruwa", label: "Polonnaruwa" },
-        { key: "puttalam", label: "Puttalam" },
-        { key: "ratnapura", label: "Ratnapura" },
-        { key: "trincomalee", label: "Trincomalee" },
-        { key: "vavuniya", label: "Vavuniya" },
+        {key: "work from home", label: "Work From Home"},
+        {key: "island wide", label: "Island Wide"},
+        {key: "ampara", label: "Ampara"},
+        {key: "anuradhapura", label: "Anuradhapura"},
+        {key: "badulla", label: "Badulla"},
+        {key: "batticaloa", label: "Batticaloa"},
+        {key: "colombo", label: "Colombo"},
+        {key: "galle", label: "Galle"},
+        {key: "gampaha", label: "Gampaha"},
+        {key: "hambantota", label: "Hambantota"},
+        {key: "jaffna", label: "Jaffna"},
+        {key: "kalutara", label: "Kalutara"},
+        {key: "kandy", label: "Kandy"},
+        {key: "kegalle", label: "Kegalle"},
+        {key: "kilinochchi", label: "Kilinochchi"},
+        {key: "kurunegala", label: "Kurunegala"},
+        {key: "mannar", label: "Mannar"},
+        {key: "matale", label: "Matale"},
+        {key: "matara", label: "Matara"},
+        {key: "monaragala", label: "Monaragala"},
+        {key: "mullativu", label: "Mullativu"},
+        {key: "nuwara eliya", label: "Nuwara Eliya"},
+        {key: "polonnaruwa", label: "Polonnaruwa"},
+        {key: "puttalam", label: "Puttalam"},
+        {key: "ratnapura", label: "Ratnapura"},
+        {key: "trincomalee", label: "Trincomalee"},
+        {key: "vavuniya", label: "Vavuniya"},
     ];
     const jobTypes = [
-        { key: "full time", label: "Full-time" },
-        { key: "part time", label: "Part-time" },
-        { key: "contract", label: "Contract" },
-        { key: "other", label: "Other" },
+        {key: "full time", label: "Full-time"},
+        {key: "part time", label: "Part-time"},
+        {key: "contract", label: "Contract"},
+        {key: "other", label: "Other"},
 
     ];
-    const fieldValues=[
-        { key: 1, label: "Account & Finance" },
-        { key: 2, label: "Administration / Secretarial" },
-        { key: 3, label: "Agriculture" },
-        { key: 4, label: "Apparel" },
-        { key: 5, label: "Architecture" },
-        { key: 6, label: "Automobile" },
-        { key: 7, label: "Banking and Financial Services" },
-        { key: 8, label: "Beauty & Hairdressing" },
-        { key: 9, label: "BPO/ KPO" },
-        { key: 10, label: "Building & Construction" },
-        { key: 11, label: "Business Management" },
-        { key: 12, label: "Call Center" },
-        { key: 13, label: "Charity / NGO" },
-        { key: 14, label: "Customer Service" },
-        { key: 15, label: "Delivery / Driving / Transport" },
-        { key: 16, label: "Digital Marketing" },
-        { key: 17, label: "Education / Higher Education" },
-        { key: 18, label: "Electronics / Electrical" },
-        { key: 19, label: "Engineering / Manufacturing" },
-        { key: 20, label: "Environment/ Health & Safety" },
-        { key: 21, label: "FMCG/ Food Industry" },
-        { key: 23, label: "Government/ Public Sector" },
-        { key: 24, label: "Hospital/ Nursing/ Healthcare" },
-        { key: 25, label: "Hotel/ Hospitality/ Leisure" },
-        { key: 26, label: "Human Resources / Recruitment" },
-        { key: 27, label: "Insurance" },
-        { key: 28, label: "Interior Design" },
-        { key: 29, label: "Internship / Undergraduate" },
-        { key: 30, label: "IT-HWare/ Networks/ Systems" },
-        { key: 31, label: "IT-SWare / Internet" },
-        { key: 32, label: "Legal / Law" },
-        { key: 33, label: "Media/ Advertising/ Communication/ Design" },
-        { key: 34, label: "Oil, Gas and Nuclear" },
-        { key: 35, label: "Other" },
-        { key: 36, label: "Pharmaceutical" },
-        { key: 37, label: "Production & Operations" },
-        { key: 38, label: "Project Management / Programme Management" },
-        { key: 39, label: "Quality Assurance" },
-        { key: 40, label: "Real Estate" },
-        { key: 41, label: "Recoveries" },
-        { key: 42, label: "Retail / Fashion" },
-        { key: 43, label: "Sales / Marketing / New Business Development" },
-        { key: 44, label: "School Leavers" },
-        { key: 45, label: "Science / Research" },
-        { key: 46, label: "Security/ Military" },
-        { key: 47, label: "Senior Management / Directors" },
-        { key: 48, label: "Sports/Fitness/Recreation" },
-        { key: 49, label: "Startup/ Tech-startup" },
-        { key: 50, label: "Supply Chain / Logistics / Procurement" },
-        { key: 51, label: "Technical/ Mechanical" },
-        { key: 52, label: "Telecommunications" },
-        { key: 53, label: "Training and Development" },
-        { key: 54, label: "Travel/Ticketing/Airline/Shipping" },
-        { key: 55, label: "Volunteer / Unpaid" },
+
+    const experienceLevelTypes=[
+        {key: 1, label: "6 Months"},
+        {key: 2, label: "1-2 Years"},
+        {key: 3, label: "2-3 Years"},
+        {key: 4, label: "3-5 Years"},
+    ]
+
+    const educationLevelTypes=[
+        {key: 1, label: "Ordinary Level"},
+        {key: 2, label: "Advanced Level"},
+        {key: 3, label: "Bachelor’s Degree"},
+        {key: 4, label: "Masters Degree"},
+        {key: 5, label: "PhD"},
+        {key: 6, label: "Diploma/HND"},
+        {key: 7, label: "Certificate"},
+    ]
+
+    const fieldValues = [
+        {key: 1, label: "Account & Finance"},
+        {key: 2, label: "Administration / Secretarial"},
+        {key: 3, label: "Agriculture"},
+        {key: 4, label: "Apparel"},
+        {key: 5, label: "Architecture"},
+        {key: 6, label: "Automobile"},
+        {key: 7, label: "Banking and Financial Services"},
+        {key: 8, label: "Beauty & Hairdressing"},
+        {key: 9, label: "BPO/ KPO"},
+        {key: 10, label: "Building & Construction"},
+        {key: 11, label: "Business Management"},
+        {key: 12, label: "Call Center"},
+        {key: 13, label: "Charity / NGO"},
+        {key: 14, label: "Customer Service"},
+        {key: 15, label: "Delivery / Driving / Transport"},
+        {key: 16, label: "Digital Marketing"},
+        {key: 17, label: "Education / Higher Education"},
+        {key: 18, label: "Electronics / Electrical"},
+        {key: 19, label: "Engineering / Manufacturing"},
+        {key: 20, label: "Environment/ Health & Safety"},
+        {key: 21, label: "FMCG/ Food Industry"},
+        {key: 23, label: "Government/ Public Sector"},
+        {key: 24, label: "Hospital/ Nursing/ Healthcare"},
+        {key: 25, label: "Hotel/ Hospitality/ Leisure"},
+        {key: 26, label: "Human Resources / Recruitment"},
+        {key: 27, label: "Insurance"},
+        {key: 28, label: "Interior Design"},
+        {key: 29, label: "Internship / Undergraduate"},
+        {key: 30, label: "IT-HWare/ Networks/ Systems"},
+        {key: 31, label: "IT-SWare / Internet"},
+        {key: 32, label: "Legal / Law"},
+        {key: 33, label: "Media/ Advertising/ Communication/ Design"},
+        {key: 34, label: "Oil, Gas and Nuclear"},
+        {key: 35, label: "Other"},
+        {key: 36, label: "Pharmaceutical"},
+        {key: 37, label: "Production & Operations"},
+        {key: 38, label: "Project Management / Programme Management"},
+        {key: 39, label: "Quality Assurance"},
+        {key: 40, label: "Real Estate"},
+        {key: 41, label: "Recoveries"},
+        {key: 42, label: "Retail / Fashion"},
+        {key: 43, label: "Sales / Marketing / New Business Development"},
+        {key: 44, label: "School Leavers"},
+        {key: 45, label: "Science / Research"},
+        {key: 46, label: "Security/ Military"},
+        {key: 47, label: "Senior Management / Directors"},
+        {key: 48, label: "Sports/Fitness/Recreation"},
+        {key: 49, label: "Startup/ Tech-startup"},
+        {key: 50, label: "Supply Chain / Logistics / Procurement"},
+        {key: 51, label: "Technical/ Mechanical"},
+        {key: 52, label: "Telecommunications"},
+        {key: 53, label: "Training and Development"},
+        {key: 54, label: "Travel/Ticketing/Airline/Shipping"},
+        {key: 55, label: "Volunteer / Unpaid"},
     ];
 
 
@@ -179,7 +206,6 @@ const JobPost = () => {
                 </div>
 
 
-
                 <div className={"mb-1 mx-2 lg:w-[45%] w-full"}>
                     <label htmlFor={"locations"}>
                         Locations
@@ -236,6 +262,54 @@ const JobPost = () => {
                 </div>
 
 
+                <div className={"mb-1 mx-2 lg:w-[45%] w-full"}>
+                    <label htmlFor={"educationLevel"}>
+                        Experience Level
+                        <span className={"text-danger"}> * </span>
+                    </label>
+                    <Select
+                        name={"experienceLevel"}
+                        items={experienceLevelTypes}
+                        placeholder="Select the experience level"
+                        variant={"bordered"}
+                        {...register("experienceLevel", {
+                            required: {
+                                value: true,
+                                message: "Please choose the experience level",
+                            }
+                        })}
+                    >
+                        {(experienceLevelType) => <SelectItem>{experienceLevelType.label}</SelectItem>}
+                    </Select>
+                    <span className="mt-3 text-danger text-sm">
+        {errors && errors["experienceLevel"] ? errors["experienceLevel"]?.message?.toString() : '\u00A0'}
+             </span>
+                </div>
+                <div className={"mb-1 mx-2 lg:w-[45%] w-full"}>
+                    <label htmlFor={"educationLevel"}>
+                        Education Level
+                        <span className={"text-danger"}> * </span>
+                    </label>
+                    <Select
+                        name={"educationLevel"}
+                        items={educationLevelTypes}
+                        placeholder="Select the education level"
+                        variant={"bordered"}
+                        {...register("educationLevel", {
+                            required: {
+                                value: true,
+                                message: "Please choose the education level",
+                            }
+                        })}
+                    >
+                        {(educationLevelType) => <SelectItem>{educationLevelType.label}</SelectItem>}
+                    </Select>
+                    <span className="mt-3 text-danger text-sm">
+        {errors && errors["educationLevel"] ? errors["educationLevel"]?.message?.toString() : '\u00A0'}
+             </span>
+                </div>
+
+
                 <CustomTextArea
                     className={"w-full"}
                     name={"overview"}
@@ -253,31 +327,62 @@ const JobPost = () => {
                         }
                     }/>
 
+                <div className={"mb-1 mx-2 w-full"}>
+                    <label htmlFor={"description"}>
+                        Description <small>( Format options are available)</small>
+                        <span className={"text-danger"}> * </span>
+                    </label>
+                    <div className={"w-full h-96 border border-gray-300 rounded-lg overflow-auto "}>
+                        <BlockNoteView
+                            theme={theme}
+                            editor={editor}/>
+                    </div>
+                    <span className="mt-3 text-danger text-sm">
+        {'\u00A0'}
+             </span>
+                </div>
 
+                <div className={"mb-1 mx-2 lg:w-[45%] w-full"}>
+                    <label htmlFor={"deadline"}>
+                        Deadline
+                        <span className={"text-danger"}> * </span>
+                    </label>
+                    <Input
+                        id="deadline"
+                        variant="bordered"
+                        type={"date"}
+                        {...register('deadline', {
+                            required: {
+                                value: true,
+                                message: "Please choose the deadline",
+                            },
+                        })}/>
 
+                    <span className="mt-3 text-danger text-sm">
+        {errors && errors["deadline"] ? errors["deadline"]?.message?.toString() : '\u00A0'}
+             </span>
+                </div>
+
+                <div className={"mb-1 mx-2 lg:w-[45%] w-full"}>
+                    <label htmlFor={"image"}>
+                        Image <small>Description or poster</small>
+                        <span className={"text-danger"}> * </span>
+                    </label>
+                    <Input
+                        id="image"
+                        variant="bordered"
+                        type={"file"}
+
+                    />
+
+                    <span className="mt-3 text-danger text-sm">
+        {errors && errors["image"] ? errors["image"]?.message?.toString() : '\u00A0'}
+             </span>
+                </div>
+
+                <Button color={"primary"} >Create job vacancy</Button>
             </div>
-        {/*    <div className={"mb-1 mx-2"}>*/}
-        {/*        <label htmlFor={"overview"}>*/}
-        {/*            Overview*/}
-        {/*            <span className={"text-danger"}> * </span>*/}
-        {/*        </label>*/}
-        {/*        <Textarea*/}
-        {/*            id={"overview"}*/}
-        {/*            name={"overview"}*/}
-        {/*            placeholder="Enter your description"*/}
-        {/*            variant={"bordered"}*/}
-        {/*            {...register("overview", {*/}
-        {/*                required: {*/}
-        {/*                    value: true,*/}
-        {/*                    message: "Please enter a job overview",*/}
-        {/*                }*/}
-        {/*            })}*/}
-        {/*        />*/}
-        {/*        <span className="mt-3 text-danger text-sm">*/}
-        {/*{errors && errors["overview"] ? errors["overview"]?.message?.toString() : '\u00A0'}*/}
-        {/*     </span>*/}
-        {/*    </div>*/}
-            <pre>{JSON.stringify(watch(), null, 2)}</pre>
+            {/*<pre>{JSON.stringify(watch(), null, 2)}</pre>*/}
 
         </div>
     );
