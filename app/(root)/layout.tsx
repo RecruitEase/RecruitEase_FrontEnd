0452105a8e4c063a07f386e4cba8887b609183d3
chathura226 <chathuralakshan226@gmail.com>
2024-07-11 18:01:54 +0530

    basic additions and ui

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,17 +1,17 @@
-import { Navbar } from "@/components/navbar";
+import {Navbar} from "@/components/navbar";
 import SideBar from "@/components/dashboard/SideBar";
-import { Link } from "@nextui-org/link";
+import {Link} from "@nextui-org/link";
 
 export default function RootLayout({
-    children,
-}: Readonly<{
+                                       children,
+                                   }: Readonly<{
     children: React.ReactNode;
 }>) {
     return (
         <>
             <Navbar />
-            <main className="flex h-screen w-full">
-                {children}
+                <main className="mx-[5rem] mx-auto">
+                    {children}
             </main>
 
         </>
