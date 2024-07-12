0ade8268becef8c56a9e394fe527c27f9b78f931
chathura226 <chathuralakshan226@gmail.com>
2024-07-12 18:43:36 +0530

    aplicant track finished

--- components/recruiter/ApplicationComponent.tsx
+++ components/recruiter/ApplicationComponent.tsx
@@ -1,18 +1,22 @@
 "use client"
 import React, {useEffect} from "react";
-import {Tabs, Tab} from "@nextui-org/react";
+import {Tabs, Tab, SelectItem, Select} from "@nextui-org/react";
 import {Card, CardHeader, CardBody, Image} from "@nextui-org/react";
 
 import {useMediaQuery} from "react-responsive";
 import {FaTag} from "react-icons/fa";
-import { MdOutlineMail ,MdOutlineLocationOn } from "react-icons/md";
+import {MdOutlineMail, MdOutlineLocationOn} from "react-icons/md";
+import {Button} from "@nextui-org/button";
+import ApplicationTable from "@/components/recruiter/ApplicationTable";
 
 
 export default function ApplicationComponent() {
-    const applicant={
+    const applicant = {
         name: "Mary Jane",
         city: "Colombo",
-        email:"maryjane@gmail.com"
+        email: "maryjane@gmail.com",
+        status: "underReview",
+        appliedDate: "2024-10-10",
     }
 
     const [isVertical, setIsVertical] = React.useState(true);
@@ -30,6 +34,7 @@ export default function ApplicationComponent() {
 
     const tabList = [
         {key: 'allApplicants', title: 'All Applicants', color: '#1E3A8A'}, // Light Blue
+        {key: 'underReview', title: 'Under Review', color: '#59cfa6'}, // Light Blue
         {key: 'preScreeningPassed', title: 'Pre-Screening Passed', color: '#827717'}, // Light Green
         {key: 'preScreeningFailed', title: 'Pre-Screening Failed', color: '#B71C1C'}, // Light Red
         {key: 'shortlisted', title: 'Shortlisted', color: '#F57F17'}, // Light Yellow
@@ -43,35 +48,50 @@ export default function ApplicationComponent() {
 
 
     return (
-        <div className="flex flex-col px-4">
+        <div className="flex flex-col px-4 mb-[100px]">
             <div className="flex w-full flex-col">
-                <Tabs color={"primary"} aria-label="Options" isVertical={isVertical}>
+                <Tabs className={"mb-5"} color={"primary"} aria-label="Options" isVertical={isVertical}>
 
                     {tabList.map((tab) => (
                         <Tab className={"h-10 w-full"} key={tab.key}
                              title={<>{tab.title} &nbsp; <FaTag style={{"fill": tab.color, "display": "unset"}}/></>}>
-                            <Card className={"w-full bg-transparent"}>
+                            <Card className={"w-full bg-transparent border"}>
                                 <CardBody>
                                     <div
-                                        className="grid grid-cols-12 gap-2 items-center justify-center">
-                                        <div className="relative col-span-5 ">
-                                            <Image
-                                                alt="Album cover"
-                                                className="object-cover"
-                                                height={200}
-                                                shadow="md"
-                                                src="https://nextui.org/images/album-cover.png"
-                                                width="100%"
-                                            />
+                                        className="grid grid-cols-12 gap-1 items-center justify-center">
+                                        <div className="relative col-span-7 ">
+                                            <ApplicationTable/>
                                         </div>
                                         <div
-                                            className="relative col-span-7  bg-transparent rounded-lg overflow-hidden shadow-lg">
-                                            <div className="border-b px-4 pb-6">
-                                                <div>
-                                                    <FaTag style={{"fill": tab.color, "display": "unset"}}/>
+                                            className="relative col-span-5  bg-transparent rounded-lg overflow-hidden shadow-lg ">
+                                            <div className={"flex  flex-col gap-2 justify-center items-center"}>
+                                                <Select
+                                                    label={"Move to"}
+                                                    placeholder="Select a status"
+                                                    className="max-w-xs col-span-6"
+                                                    size={"sm"}
+                                                    defaultSelectedKeys={["underReview"]}
+                                                >
+                                                    {tabList.map((tab) => (
+                                                        <SelectItem key={tab.key}>
+                                                            {tab.title}
+                                                        </SelectItem>
+                                                    ))}
+                                                </Select>
+                                                <div className={"col-span-6 flex justify-end"}>
+                                                    <FaTag style={{
+                                                        "fill": tabList.find(obj => obj.key == applicant.status).color,
+                                                        "display": "unset"
+                                                    }}/>
                                                     &nbsp;
                                                     <span
-                                                        className="text-sm font-semibold text-gray-800 dark:text-white">{tab.title}</span>
+                                                        className="text-sm font-semibold text-gray-800 dark:text-white">{tabList.find(obj => obj.key == applicant.status).title}</span>
+                                                </div>
+                                            </div>
+                                            <div className="border-b px-4 pb-6">
+                                                <div className={"grid grid-cols-12 gap-2 items-center justify-center"}>
+
+
                                                 </div>
                                                 <div className="text-center my-4">
                                                     <img
@@ -82,12 +102,13 @@ export default function ApplicationComponent() {
                                                         <h3 className="font-bold text-2xl text-gray-800 dark:text-white mb-1">{applicant.name}</h3>
                                                         <div
                                                             className="inline-flex text-gray-700 dark:text-gray-300 items-center">
-                                                            <MdOutlineLocationOn />&nbsp;
+                                                            <MdOutlineLocationOn/>&nbsp;
                                                             {applicant.city}
-                                                        </div><br/>
+                                                        </div>
+                                                        <br/>
                                                         <div
                                                             className="inline-flex text-gray-700 dark:text-gray-300 items-center">
-                                                            <MdOutlineMail />
+                                                            <MdOutlineMail/>
                                                             &nbsp;
                                                             {applicant.email}
                                                         </div>
@@ -99,7 +120,7 @@ export default function ApplicationComponent() {
                                                         View CV
                                                     </button>
                                                     <button
-                                                        className="flex-1 rounded-full border-2 border-gray-400 dark:border-gray-700 font-semibold text-black dark:text-white px-4 py-2">
+                                                        className="flex-1 rounded-full border-2 border-gray-400 dark:border-gray-700 font-semibold text-black hover:bg-blue-600 hover:text-white dark:text-white px-4 py-2">
                                                         Message
                                                     </button>
                                                 </div>
@@ -107,14 +128,25 @@ export default function ApplicationComponent() {
                                             <div className="px-4 py-4">
                                                 <div
                                                     className="flex gap-2 items-center text-gray-800 dark:text-gray-300 mb-4">
-                                                    <svg className="h-6 w-6 text-gray-600 dark:text-gray-400"
-                                                         fill="currentColor"
-                                                         xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
-                                                         width="24" height="24">
-                                                        <path className=""
-                                                              d="M12 12a5 5 0 1 1 0-10 5 5 0 0 1 0 10zm0-2a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm9 11a1 1 0 0 1-2 0v-2a3 3 0 0 0-3-3H8a3 3 0 0 0-3 3v2a1 1 0 0 1-2 0v-2a5 5 0 0 1 5-5h8a5 5 0 0 1 5 5v2z"/>
-                                                    </svg>
-                                                    <span><strong className="text-black dark:text-white">12</strong> Followers you know</span>
+                                                    <div className={"w-1/2 flex flex-col justify-center items-center gap-2"}>
+                                                        <Button size={"sm"} className={"w-full bg-purple-500 text-white"}>
+                                                            View Answers
+                                                        </Button>
+
+                                                        <Button size={"sm"} color={"secondary"}
+                                                                className={" w-full bg-purple-500 text-white"}>
+                                                            Schedule Interview
+                                                        </Button>
+
+                                                        <Button size={"sm"} color={"secondary"}
+                                                                className={"w-full bg-purple-500 text-white"}>
+                                                            More Actions
+                                                        </Button>
+                                                    </div>
+                                                    <div className="flex items-center flex-col justify-center">
+                                                        <small>Application Date: </small>
+                                                        <small><b>{ applicant.appliedDate}</b></small>
+                                                    </div>
                                                 </div>
 
                                             </div>
