98d81e97cb8c25187308cb42132dbf73135666fe
chathura226 <chathuralakshan226@gmail.com>
2024-07-13 22:39:26 +0530

    de

---
+++ app/(newDashoard)/recruiter/applications/page.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import HeaderBox from "@/components/dashboard/HeaderBox";
+import ApplicationComponent from '@/components/recruiter/ApplicationComponent';
+
+const Applications = () => {
+
+    // @ts-ignore
+    return (
+        <div>
+            <header className="home-header">
+                <HeaderBox
+                    type="title"
+                    title="Applicantions"
+                    subtext="Manage the applications received for the job from here"
+                />
+            </header>
+            <ApplicationComponent/>
+
+        </div>
+    );
+};
+
+export default Applications;
