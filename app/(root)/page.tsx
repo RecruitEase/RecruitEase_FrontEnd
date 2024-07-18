27cf3e2e2d07d8d817132e9379c42344755c94b1
chathura226 <chathuralakshan226@gmail.com>
2024-07-18 13:04:34 +0530

    home-1

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,14 +1,20 @@
 "use client"
 import React from 'react';
-import { Button } from "@nextui-org/button";
-import { signIn, signOut, useSession } from "next-auth/react";
+import {Button} from "@nextui-org/button";
+import {signIn, signOut, useSession} from "next-auth/react";
 import useAxiosAuth from "@/lib/hooks/useAxiosAuth";
-import { toast, ToastContainer } from "react-toastify";
+import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import Hero from "@/components/home/Hero";
+import Apps from "@/components/home/Apps";
+import Comparison from "@/components/home/Comparison";
+import Stats from "@/components/home/Stats";
+import People from "@/components/home/People";
+import Footer from "@/components/home/Footer";
 
 const Home2 = () => {
-    const { data: session } = useSession();
-    console.log({ session });
+    const {data: session} = useSession();
+    console.log({session});
 
     const axios = useAxiosAuth();
 
@@ -20,8 +26,21 @@ const Home2 = () => {
 
     const notify = () => toast("Wow so easy!");
     return (
-        <div>
-            HOme
+        <div className={"relative"}>
+            <div className={"max-w-screen-xl mx-auto overflow-hidden"}>
+                <Hero/>
+                <Apps/>
+                <Comparison/>
+                <Stats/>
+                <People/>
+            </div>
+            <Footer/>
+            <svg className='absolute top-0 left-0 bottom-0 right-0 opacity-20 ' width="100%" height="100%">
+                <pattern id="smallGrid" width="25" height="25" patternUnits="userSpaceOnUse">
+                    <circle cx="10" cy="10" r="1" fill="#fff"/>
+                </pattern>
+                <rect width="100%" height="100%" fill="url(#smallGrid)"/>
+            </svg>
         </div>
     );
 };
