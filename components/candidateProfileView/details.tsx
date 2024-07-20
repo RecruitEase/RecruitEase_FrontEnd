85d104e613334274f8cf619321b7a8f3ee467bea
Sajith-Bandara <sajithbandara091@gmail.com>
2024-07-20 17:22:13 +0530

    candidate profile view created

---
+++ components/candidateProfileView/details.tsx
@@ -0,0 +1,134 @@
+"use client";
+
+import React, { useState } from "react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image, Button } from "@nextui-org/react";
+import { Avatar } from "@nextui-org/react";
+import { Chip } from "@nextui-org/react";
+import Carousel from "@/components/carousel";
+import { ScrollShadow } from "@nextui-org/react";
+import carousel from "@/components/carousel";
+
+interface PersonalDetailsProps {
+    aboutMe: string;
+    skills: string[];
+    experience: string[];
+    education: string[];
+    location: string;
+    email: string;
+    avatars: string[];
+    carousel:string[];
+}
+
+const PersonalDetails: React.FC<PersonalDetailsProps> = ({ aboutMe, skills, experience, education, location, email, avatars,carousel }) => {
+    const [isExpanded, setIsExpanded] = useState(false);
+
+    const toggleExpand = () => {
+        setIsExpanded(!isExpanded);
+    };
+
+    return (
+        <div className="flex gap-4 flex-col lg:flex-row">
+            <div className="aboutme">
+                <Card className="min-w-[400px] flex-1">
+                    <CardHeader className="flex gap-3">
+                        <p className="text-md font-bold">About me</p>
+                    </CardHeader>
+                    <Divider />
+                    <ScrollShadow className="w-full h-[120px] pl-4">
+                        {aboutMe}
+                    </ScrollShadow>
+                    <Divider />
+                </Card>
+
+                <Card className="min-w-[400px] flex-1 mt-4">
+                    <CardHeader>
+                        <p className="text-md font-bold">Skills</p>
+                    </CardHeader>
+                    <Divider />
+                    <CardBody>
+                        <ScrollShadow className="w-full h-[90px] pl-4">
+                            <div className="flex flex-row flex-wrap gap-2">
+                                {skills.map((skill, index) => (
+                                    <Chip key={index} size="lg" className="w-2">{skill}</Chip>
+                                ))}
+                            </div>
+                        </ScrollShadow>
+                    </CardBody>
+                    <Divider />
+
+                    <CardHeader>
+                        <p className="text-md font-bold">Experience</p>
+                    </CardHeader>
+                    <Divider />
+                    <CardBody>
+                        <ScrollShadow className="w-full h-[138px] pl-4">
+                            {experience.map((item, index) => (
+                                <p key={index}>{item}</p>
+                            ))}
+                        </ScrollShadow>
+                    </CardBody>
+                    <Divider />
+
+                    <CardHeader>
+                        <p className="text-md font-bold">Education</p>
+                    </CardHeader>
+                    <Divider />
+                    <CardBody>
+                        <ScrollShadow className="w-full h-[100px] pl-4">
+                            {education.map((item, index) => (
+                                <p key={index}>{item}</p>
+                            ))}
+                        </ScrollShadow>
+                    </CardBody>
+                    <Divider />
+                </Card>
+            </div>
+
+            <div className="details">
+                <Card className="min-w-[400px] flex-1">
+                    <CardHeader className="flex gap-3">
+                        <p className="text-md font-bold">Location</p>
+                    </CardHeader>
+                    <Divider />
+                    <CardBody>
+                        <p>{location}</p>
+                    </CardBody>
+                    <Divider />
+
+                    <CardHeader className="flex gap-3">
+                        <p className="text-md font-bold">Email</p>
+                    </CardHeader>
+                    <Divider />
+                    <CardBody>
+                        <p>{email}</p>
+                    </CardBody>
+                    <Divider />
+
+                    <CardBody>
+                        <div className="flex gap-4 items-center">
+                            {avatars.map((avatar, index) => (
+                                <Avatar key={index} isBordered radius="full" src={avatar} />
+                            ))}
+                        </div>
+                    </CardBody>
+                    <Divider />
+                </Card>
+
+                <Card className="min-w-[400px] flex-1 mt-4">
+                    <CardHeader className="flex gap-3">
+                        <p className="text-md font-bold">My CV</p>
+                    </CardHeader>
+                    <Divider />
+                    <CardBody>
+                        <div>
+                            <Carousel images={carousel}/>
+                        </div>
+                    </CardBody>
+                    <Divider />
+                </Card>
+            </div>
+        </div>
+    );
+};
+
+export default PersonalDetails;
---
+++ components/candidateProfileView/profileImageCard.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { Card, CardHeader, CardBody, CardFooter, Divider, Link, Image } from "@nextui-org/react";
+
+
+// @ts-ignore
+const ProfileImage=({profileData})=> {
+    return (
+        <div className="profile-page flex flex-row flex-wrap">
+            <div className={" min-w-[50px] "}>
+                <Image
+                    alt="nextui logo"
+                    style={{width: 120, height: 'auto', aspectRatio: 1}}
+                    radius="full"
+                    src={profileData.imageUrl}/>
+            </div>
+            <div className={"flex flex-col pl-4 justify-center"}>
+                <p className="text-26">{profileData.name}</p>
+                <p className="text-large text-default-500">{profileData.thumbnail}</p>
+            </div>
+
+            {/*<Card className="w-full min-w-[400px] shadow-none p-0">*/}
+            {/*    <CardHeader className="flex gap-3 pl-0">*/}
+            {/*        <Image*/}
+            {/*            alt="nextui logo"*/}
+            {/*            style={{ width: 120, height: 'auto', aspectRatio: 1 }}*/}
+            {/*            radius="full"*/}
+            {/*            src={profileData.imageUrl}*/}
+
+            {/*        />*/}
+            {/*        <div className="flex flex-col pl-4">*/}
+            {/*            <p className="text-26">{profileData.name}</p>*/}
+            {/*            <p className="text-large text-default-500">{profileData.thumbnail}</p>*/}
+            {/*        </div>*/}
+            {/*    </CardHeader>*/}
+            {/*</Card>*/}
+
+        </div>
+    );
+}
+
+export default ProfileImage;
