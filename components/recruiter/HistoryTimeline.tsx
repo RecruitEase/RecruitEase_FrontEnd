e67b5624f5f14a00f5257ac6814ae65d4f42a5da
raviyax <2021is043@stu.ucsc.cmb.ac.lk>
2024-07-24 13:19:14 +0530

    modified:   components/recruiter/HistoryTimeline.tsx

--- components/recruiter/HistoryTimeline.tsx
+++ components/recruiter/HistoryTimeline.tsx
@@ -16,9 +16,9 @@ export default function HistoryTimeline() {
               Senior Frontend Developer
             </h3>
           </div>
-          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          <div className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
             Last Stage : <Chip color="success">Pre-Screening</Chip>
-          </p>
+          </div>
           <a
             href="#"
             className="w-32 inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
@@ -55,9 +55,9 @@ export default function HistoryTimeline() {
               Senior Frontend Developer
             </h3>
           </div>
-          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          <div className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
             Last Stage : <Chip color="success">Pre-Screening</Chip>
-          </p>
+          </div>
           <a
             href="#"
             className="w-32 inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
@@ -94,9 +94,9 @@ export default function HistoryTimeline() {
               Senior Frontend Developer
             </h3>
           </div>
-          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          <div className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
             Last Stage : <Chip color="success">Pre-Screening</Chip>
-          </p>
+          </div>
           <a
             href="#"
             className="w-32 inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
@@ -133,9 +133,9 @@ export default function HistoryTimeline() {
               Senior Frontend Developer
             </h3>
           </div>
-          <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
+          <div className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
             Last Stage : <Chip color="success">Pre-Screening</Chip>
-          </p>
+          </div>
           <a
             href="#"
             className="w-32 inline-flex items-center px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:outline-none focus:ring-gray-100 focus:text-blue-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700 dark:focus:ring-gray-700"
