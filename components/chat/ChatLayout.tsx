c6c30415897532579aa3bc8fd9e5ea7ad75c39f4
chathura226 <chathuralakshan226@gmail.com>
2024-07-28 18:56:11 +0530

    send msg

--- components/chat/ChatLayout.tsx
+++ components/chat/ChatLayout.tsx
@@ -1,14 +1,14 @@
 "use client";
 import React from "react";
 import { Chip } from "@nextui-org/chip";
-import { Input, Avatar } from "@nextui-org/react";
+import { Input, Avatar, Button } from "@nextui-org/react";
 import { SearchIcon } from "../icons/searchicon";
 import { Tabs, Tab, Card, CardBody } from "@nextui-org/react";
 import { sendError } from "next/dist/server/api-utils";
 import ChatUser from "./ChatUser";
 import ChatMsg from "./ChatMsg";
 import { MsgProp, SenderProp } from "@/types";
-
+import { MailIcon } from "../icons";
 const nChats = 24;
 
 const senders: SenderProp[] = [
@@ -217,16 +217,40 @@ function ChatLayout() {
                 )}
               </div>
             </div>
-            <div className="flex flex-col gap-3 h-[720px] p-5 overflow-y-scroll">
+            <div className="flex flex-col gap-3 h-[640px] p-5 overflow-y-scroll">
+
             {messages.map((msg, index) => {
   const sender = Math.random() < 0.5 ? (logedUser) : selectedUser;
   return <ChatMsg key={index} sender={sender} msg={msg} isSenderLoggedUser={sender==logedUser}/>;
 })}
-              {/* 
-        <ChatMsg sender={logedUser} msg={msg1} isSenderLoggedUser={true} />
-        <ChatMsg sender={selectedUser} msg={msg2} />
-        <ChatMsg sender={logedUser} msg={msg2} isSenderLoggedUser={true} /> */}
+              
             </div>
+            <div className="w-full">
+            <div className="border-t-2 border-gray-200 px-4 pt-4 mb-2 sm:mb-0">
+      <div className="relative flex">
+         <span className="absolute inset-y-0 flex items-center">
+         <button type="button" className="inline-flex items-center justify-center rounded-full h-10 w-10 transition duration-500 ease-in-out text-gray-500 hover:bg-gray-300 focus:outline-none">
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" className="h-6 w-6 text-gray-600">
+                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.172 7l-6.586 6.586a2 2 0 102.828 2.828l6.414-6.586a4 4 0 00-5.656-5.656l-6.415 6.585a6 6 0 108.486 8.486L20.5 13"></path>
+               </svg>
+            </button>
+         </span>
+         <input type="text" placeholder="Write your message!" className="w-full focus:outline-none focus:placeholder-gray-400 text-gray-600 placeholder-gray-600 pl-12 bg-gray-200 rounded-md py-3 flex-wrap" />
+         <div className="absolute right-0 items-center inset-y-0 hidden sm:flex">
+            
+            <button type="button" className="inline-flex items-center justify-center rounded-lg px-4 py-3 transition duration-500 ease-in-out text-white bg-blue-500 hover:bg-blue-400 focus:outline-none">
+               <span className="font-bold">Send</span>
+               <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="h-6 w-6 ml-2 transform rotate-90">
+                  <path d="M10.894 2.553a1 1 0 00-1.788 0l-7 14a1 1 0 001.169 1.409l5-1.429A1 1 0 009 15.571V11a1 1 0 112 0v4.571a1 1 0 00.725.962l5 1.428a1 1 0 001.17-1.408l-7-14z"></path>
+               </svg>
+            </button>
+         </div>
+      </div>
+   </div>
+
+            </div>
+
+            
           </>
         )}
       </div>
