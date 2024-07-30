3211b0f190aef4bbef91917e0ac692b1513d8507
chathura226 <chathuralakshan226@gmail.com>
2024-07-30 16:07:23 +0530

    changes

--- app/(newDashoard)/recruiter/vacancy/page.tsx
+++ app/(newDashoard)/recruiter/vacancy/page.tsx
@@ -6,6 +6,7 @@ import FilterBar from "@/components/recruiter/FilterBar";
 import DashboardCards from "@/components/recruiter/DashboardCards";
 import VacancyTable from "@/components/recruiter/VacancyTable";
 import { Button } from "@nextui-org/button";
+import Link from "next/link";
 
 const vacancies = () => {
   return (
@@ -20,6 +21,9 @@ const vacancies = () => {
           <div className="flex flex-col gap-4">
             <div className="flex justify-end gap-3 items-end">
               <Button
+                  href={"/recruiter/post"}
+                  as={Link}
+
                 className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 me-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
                 variant="flat"
               >
--- components/sidebar/sidebar.tsx
+++ components/sidebar/sidebar.tsx
@@ -1,148 +1,187 @@
 import React from "react";
-import { Sidebar } from "./sidebar.styles";
-import { Avatar, Tooltip } from "@nextui-org/react";
-import { CompaniesDropdown } from "./companies-dropdown";
-import { HomeIcon } from "../icons/sidebar/home-icon";
-import { PaymentsIcon } from "../icons/sidebar/payments-icon";
-import { BalanceIcon } from "../icons/sidebar/balance-icon";
-import { AccountsIcon } from "../icons/sidebar/accounts-icon";
-import { CustomersIcon } from "../icons/sidebar/customers-icon";
-import { ProductsIcon } from "../icons/sidebar/products-icon";
-import { ReportsIcon } from "../icons/sidebar/reports-icon";
-import { DevIcon } from "../icons/sidebar/dev-icon";
-import { ViewIcon } from "../icons/sidebar/view-icon";
-import { SettingsIcon } from "../icons/sidebar/settings-icon";
-import { CollapseItems } from "./collapse-items";
-import { SidebarItem } from "./sidebar-item";
-import { SidebarMenu } from "./sidebar-menu";
-import { FilterIcon } from "../icons/sidebar/filter-icon";
-import { useSidebarContext } from "../layout/layout-context";
-import { ChangeLogIcon } from "../icons/sidebar/changelog-icon";
-import { usePathname } from "next/navigation";
+import {Sidebar} from "./sidebar.styles";
+import {Avatar, Tooltip} from "@nextui-org/react";
+import {CompaniesDropdown} from "./companies-dropdown";
+import {HomeIcon} from "../icons/sidebar/home-icon";
+import {PaymentsIcon} from "../icons/sidebar/payments-icon";
+import {BalanceIcon} from "../icons/sidebar/balance-icon";
+import {AccountsIcon} from "../icons/sidebar/accounts-icon";
+import {CustomersIcon} from "../icons/sidebar/customers-icon";
+import {ProductsIcon} from "../icons/sidebar/products-icon";
+import {ReportsIcon} from "../icons/sidebar/reports-icon";
+import {DevIcon} from "../icons/sidebar/dev-icon";
+import {ViewIcon} from "../icons/sidebar/view-icon";
+import {SettingsIcon} from "../icons/sidebar/settings-icon";
+import {CollapseItems} from "./collapse-items";
+import {SidebarItem} from "./sidebar-item";
+import {SidebarMenu} from "./sidebar-menu";
+import {FilterIcon} from "../icons/sidebar/filter-icon";
+import {useSidebarContext} from "../layout/layout-context";
+import {ChangeLogIcon} from "../icons/sidebar/changelog-icon";
+import {usePathname} from "next/navigation";
 import Image from "next/image";
 import {useTheme} from "next-themes";
+import { InterviewIcon } from "../icons/sidebar/Interview-icon";
+import { OffersIcon } from "../icons/sidebar/offers-icon";
 
-declare interface SideBarProps{
-  role:string;
+declare interface SideBarProps {
+    role: string;
 }
 
-export const SidebarWrapper = ({role}:SideBarProps) => {
-  const pathname = usePathname();
-  const { collapsed, setCollapsed } = useSidebarContext();
+export const SidebarWrapper = ({role}: SideBarProps) => {
+    const pathname = usePathname();
+    const {collapsed, setCollapsed} = useSidebarContext();
 
-  const {theme,setTheme}=useTheme();
-  return (
-    <aside className="h-screen z-[20] sticky top-0">
-      {collapsed ? (
-          // eslint-disable-next-line jsx-a11y/no-static-element-interactions
-        <div className={Sidebar.Overlay()} onClick={setCollapsed} />
-      ) : null}
-      <div
-        className={Sidebar({
-          collapsed: collapsed,
-        })}
-      >
-        <div className={Sidebar.Header()}>
-          { theme==='dark'?(<Image className={"w-[95%]"}
-                                   src="/logos/logoSVG.svg"
-                                   width={1511}
-                                   height={281}
-                                   alt="RecruitEase"
-          />):(<Image className={"w-[95%]"}
-                      src="/logos/logoSVGBlack.svg"
-                      width={1511}
-                      height={281}
-                      alt="RecruitEase"
-          />)}
-        </div>
-        <div className="flex flex-col justify-between h-full">
-          <div className={Sidebar.Body()}>
-            <SidebarItem
-              title={"Dashboard"}
-              icon={<HomeIcon />}
-              isActive={pathname === `/${role}`}
-              href={`/${role}`}
-            />
-            <SidebarMenu title="Main Menu">
-              <SidebarItem
-                isActive={pathname === `/${role}/profile`}
-                title="Profile"
-                icon={<AccountsIcon />}
-                href={`/${role}/profile`}
-              />
-              <SidebarItem
-                isActive={pathname === "/payments"}
-                title="Payments"
-                icon={<PaymentsIcon />}
-              />
-              <CollapseItems
-                icon={<BalanceIcon />}
-                items={["Banks Accounts", "Credit Cards", "Loans"]}
-                title="Balances"
-              />
-              <SidebarItem
-                isActive={pathname === "/customers"}
-                title="Customers"
-                icon={<CustomersIcon />}
-              />
-              <SidebarItem
-                isActive={pathname === "/products"}
-                title="Products"
-                icon={<ProductsIcon />}
-              />
-              <SidebarItem
-                isActive={pathname === "/reports"}
-                title="Reports"
-                icon={<ReportsIcon />}
-              />
-            </SidebarMenu>
+    const {theme, setTheme} = useTheme();
+    return (
+        <aside className="h-screen z-[20] sticky top-0">
+            {collapsed ? (
+                // eslint-disable-next-line jsx-a11y/no-static-element-interactions
+                <div className={Sidebar.Overlay()} onClick={setCollapsed}/>
+            ) : null}
+            <div
+                className={Sidebar({
+                    collapsed: collapsed,
+                })}
+            >
+                <div className={Sidebar.Header()}>
+                    {theme === 'dark' ? (<Image className={"w-[95%]"}
+                                                src="/logos/logoSVG.svg"
+                                                width={1511}
+                                                height={281}
+                                                alt="RecruitEase"
+                    />) : (<Image className={"w-[95%]"}
+                                  src="/logos/logoSVGBlack.svg"
+                                  width={1511}
+                                  height={281}
+                                  alt="RecruitEase"
+                    />)}
+                </div>
+                <div className="flex flex-col justify-between h-full">
+                    <div className={Sidebar.Body()}>
+                        <SidebarItem
+                            title={"Dashboard"}
+                            icon={<HomeIcon/>}
+                            isActive={pathname === `/${role}`}
+                            href={`/${role}`}
+                        />
+                        <SidebarMenu title="Main Menu">
+                            <SidebarItem
+                                isActive={pathname === `/${role}/profile`}
+                                title="Profile"
+                                icon={<AccountsIcon/>}
+                                href={`/${role}/profile`}
+                            />
 
-            <SidebarMenu title="General">
-              <SidebarItem
-                isActive={pathname === "/developers"}
-                title="Developers"
-                icon={<DevIcon />}
-              />
-              <SidebarItem
-                isActive={pathname === "/view"}
-                title="View Test Data"
-                icon={<ViewIcon />}
-              />
-              <SidebarItem
-                isActive={pathname === "/settings"}
-                title="Settings"
-                icon={<SettingsIcon />}
-              />
-            </SidebarMenu>
+                            {role == "recruiter" &&
+                                (
+                                    <>
+                                        <SidebarItem
+                                            isActive={pathname === `/${role}/subscription`}
+                                            title="Subscription"
+                                            icon={<PaymentsIcon/>}
+                                            href={`/${role}/subscription`}
+                                        />
+                                        <SidebarItem
+                                            isActive={pathname === `/${role}/vacancy`}
+                                            title="Vacancies"
+                                            icon={<ProductsIcon/>}
+                                            href={`/${role}/vacancy`}
+                                        />
+                                      <SidebarItem
+                                          isActive={pathname === `/${role}/post`}
+                                          title="Post a Job"
+                                          icon={<ReportsIcon/>}
+                                          href={`/${role}/post`}
+                                      />
+                                      <SidebarItem
+                                          isActive={pathname.startsWith(`/${role}/interviews`) }
+                                          title="Interviews"
+                                          icon={<InterviewIcon/>}
+                                          href={`/${role}/interviews`}
+                                      />
+                                        <SidebarItem
+                                            isActive={pathname === `/${role}/joboffers`}
+                                            title="Job Offers"
+                                            icon={<OffersIcon/>}
+                                            href={`/${role}/joboffers`}
+                                        />
+                                      <SidebarItem
+                                          isActive={pathname === `/${role}/tickets`}
+                                          title="Tickets"
+                                          icon={<BalanceIcon/>}
+                                          href={`/${role}/tickets`}
+                                      />
 
-            <SidebarMenu title="Updates">
-              <SidebarItem
-                isActive={pathname === "/changelog"}
-                title="Changelog"
-                icon={<ChangeLogIcon />}
-              />
-            </SidebarMenu>
-          </div>
-          <div className={Sidebar.Footer()}>
-            <Tooltip content={"Settings"} color="primary">
-              <div className="max-w-fit">
-                <SettingsIcon />
-              </div>
-            </Tooltip>
-            <Tooltip content={"Adjustments"} color="primary">
-              <div className="max-w-fit">
-                <FilterIcon />
-              </div>
-            </Tooltip>
-            <Tooltip content={"Profile"} color="primary">
-              <Avatar
-                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
-                size="sm"
-              />
-            </Tooltip>
-          </div>
-        </div>
-      </div>
-    </aside>
-  );
+                                    </>
+                                )
+                            }
+
+                            <CollapseItems
+                                icon={<BalanceIcon/>}
+                                items={["Banks Accounts", "Credit Cards", "Loans"]}
+                                title="Balances"
+                            />
+                            <SidebarItem
+                                isActive={pathname === `/${role}/chat`}
+                                title="Chats"
+                                icon={<CustomersIcon/>}
+                                href={`/${role}/chat`}
+                            />
+
+                            <SidebarItem
+                                isActive={pathname === "/reports"}
+                                title="Reports"
+                                icon={<ReportsIcon/>}
+                            />
+                        </SidebarMenu>
+
+                        <SidebarMenu title="General">
+                            <SidebarItem
+                                isActive={pathname === "/developers"}
+                                title="Developers"
+                                icon={<DevIcon/>}
+                            />
+                            <SidebarItem
+                                isActive={pathname === "/view"}
+                                title="View Test Data"
+                                icon={<ViewIcon/>}
+                            />
+                            <SidebarItem
+                                isActive={pathname === "/settings"}
+                                title="Settings"
+                                icon={<SettingsIcon/>}
+                            />
+                        </SidebarMenu>
+
+                        <SidebarMenu title="Updates">
+                            <SidebarItem
+                                isActive={pathname === "/changelog"}
+                                title="Changelog"
+                                icon={<ChangeLogIcon/>}
+                            />
+                        </SidebarMenu>
+                    </div>
+                    <div className={Sidebar.Footer()}>
+                        <Tooltip content={"Settings"} color="primary">
+                            <div className="max-w-fit">
+                                <SettingsIcon/>
+                            </div>
+                        </Tooltip>
+                        <Tooltip content={"Adjustments"} color="primary">
+                            <div className="max-w-fit">
+                                <FilterIcon/>
+                            </div>
+                        </Tooltip>
+                        <Tooltip content={"Profile"} color="primary">
+                            <Avatar
+                                src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+                                size="sm"
+                            />
+                        </Tooltip>
+                    </div>
+                </div>
+            </div>
+        </aside>
+    );
 };
