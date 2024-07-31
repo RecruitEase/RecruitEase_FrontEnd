0c5fc0a33827227defb69f5bf4ae85e2b70952a9
raviyax <2021is043@stu.ucsc.cmb.ac.lk>
2024-07-31 09:12:01 +0530

    fix

--- app/(newDashoard)/recruiter/vacancy/page.tsx
+++ app/(newDashoard)/recruiter/vacancy/page.tsx
@@ -34,7 +34,7 @@ const RecruiterDashboard = () => {
         <div className="flex flex-col gap-4">
           <div className="flex justify-end gap-3 items-end">
             <Button
-              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
+              className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-4 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800"
               variant="flat"
               onClick={() => {
                 location.href = "/recruiter/post";
