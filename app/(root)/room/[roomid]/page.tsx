c0ad581e08938aa7df740b425c53334c1a33e24c
chathura226 <chathuralakshan226@gmail.com>
2024-10-24 20:28:04 +0530

    finished streaming service with access controls

--- app/(root)/room/[roomid]/page.tsx
+++ app/(root)/room/[roomid]/page.tsx
@@ -1,15 +1,62 @@
 "use client";
 import { ZegoUIKitPrebuilt } from "@zegocloud/zego-uikit-prebuilt";
-import React from "react";
-import {useParams} from "next/navigation";
+import React, {useEffect, useState} from "react";
+import {useParams, useRouter} from "next/navigation";
 import {useSession} from "next-auth/react";
 import { v4 as uuid } from "uuid";
+import {getInterviewById} from "@/lib/api";
+import {Bounce, toast} from "react-toastify";
+import LoadingComponent from "@/components/LoadingComponent";
 
 const Room = () => {
+    const [hasAccess,setHasAccess]=useState(false)
     const { data: session } = useSession();
     const fullName = session?.user?.roleDetails.firstName+" "+session?.user?.roleDetails.lastName || "Guest";
     const params = useParams<{roomid:string}>()
-    const roomID = params.roomid;
+    const roomID = params.roomid; //room id is interview id. so check whether the logged in user have access
+    const router=useRouter()
+    useEffect(() => {
+        const res=getInterviewById(roomID);
+        res.then((response)=>{
+            //if role is candidate check candidateid
+            //if role is recruiter check recruiterId
+            if(session?.user.role=="candidate" && response.candidateId==session.user.roleDetails.candidateId){
+                    setHasAccess(true)
+            }else if(session?.user.role=="recruiter" && response.recruiterId==session.user.roleDetails.recruiterId){
+                setHasAccess(true)
+            }else{
+                //unauthorized. redirect to home page
+                toast.error('Unauthorized! ', {
+                    position: "top-right",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                    transition: Bounce,
+                });
+                router.push('/');
+            }
+            console.log(response)
+        }).catch((e)=>{
+            console.log(e)
+            toast.error('Error occurred! ', {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+                transition: Bounce,
+            });
+            router.push('/');
+        })
+
+    }, [roomID]);
 
     let myMeeting: any = async (element: any) => {
         // generate Kit Token
@@ -48,7 +95,11 @@ const Room = () => {
         });
     };
 
-    return <div className="w-full h-screen" ref={myMeeting}></div>;
+    if(hasAccess){
+        return <div className="w-full h-screen" ref={myMeeting}></div>;
+    }else{
+        return <LoadingComponent />
+    }
 };
 
 export default Room;
