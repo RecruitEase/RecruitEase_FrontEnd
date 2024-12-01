1ec216372bd2a1e2e50370e9ccfa231c3f6cc0f1
Sajith-Bandara <sajithbandara091@gmail.com>
2024-12-01 16:00:54 +0530

    modified ticket view moderator side

--- app/(newDashoard)/moderator/candidate-profile/[id]/page.tsx
+++ app/(newDashoard)/moderator/candidate-profile/[id]/page.tsx
@@ -8,15 +8,11 @@ import { useState, useEffect } from 'react';
 import { useParams } from 'next/navigation';
 import { getCandidate } from '@/lib/api';
 import { CandidateProp } from '@/types/users';
-import LoadingComponent from "@/components/LoadingComponent";
-import ErrorComponent from "@/components/ErrorComponent";
-
 
 const Profile = () => {
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(false);
   const params = useParams();
 
+  const [userData, setUserData] = useState<CandidateProp>();
   const [profileData, setProfileData] = useState({ name: "", email: "", location: "", imageUrl: "" });
   const [data, setData] = useState({
     aboutMe: "", skills: [], experience: [], education: [], location: "", email: ""
@@ -25,53 +21,36 @@ const Profile = () => {
   const candidateId: string = (params?.id as string);
 
   useEffect(() => {
-    setError(false);
-    setLoading(true);
-    try {
+    const res = getCandidate(candidateId);
+    res.then((data) => {
+      setUserData(data);
+    });
+  })
 
-      const res = getCandidate(candidateId);
-      res.then((data) => {
-        setProfileData({
-          name: data?.firstName + " " + data?.lastName,
-          email: data?.email || "",
-          location: data?.city || "",
-          imageUrl: data?.profilePic || "" });
-        setData({
-          aboutMe: data?.aboutMe || "",
-          skills: JSON.parse(data?.skills || '[]'),
-          experience: JSON.parse(data?.experience || '[]'),
-          education: JSON.parse(data?.education || '[]'),
-          location: data?.address || "",
-          email: data?.email || ""
-        });
-        setLoading(false);
-      });
-    }catch(e){
-      setLoading(false);
-      console.log(e);
-      setError(true);
-    }
-  }, [candidateId])
+  useEffect(() => {
+    setProfileData({ name: userData?.firstName + " " + userData?.lastName, email: userData?.email || "", location: userData?.city || "", imageUrl: userData?.profilePic || "" });
+    setData({ aboutMe: userData?.aboutMe || "", skills: JSON.parse(userData?.skills || '[]'), experience: JSON.parse(userData?.experience || '[]'), education: JSON.parse(userData?.education || '[]'), location: userData?.address || "", email: userData?.email || "" });
+    // console.log("aaaaaaaaaaaaaakkkkkkkklllla", userData);
+  }, [userData]);
 
   return (
-      (loading) ? <LoadingComponent /> :
-          (error) ? <ErrorComponent /> :
-              <>
-                <div className="flex justify-end">
-                  <Button
-                      color={"secondary"}
-                      className={"w-fit bg-gray-900 text-whiteText"}
-                      as={Link}
-                      href="/recruiter/candidate-profile/abc123/history"
-                  >
-                    Applicant History <FaHistory />
-                  </Button>
-                </div>
+      <><div className="flex justify-end"><Button
+          color={"secondary"}
+          className={"w-fit bg-gray-900 text-whiteText"}
+          as={Link}
+          href="/recruiter/candidate-profile/abc123/history"
+      >
+        Applicant History <FaHistory />
+      </Button>
+      </div>
 
-                <div className={"pb-4"}><ProfileImageCard profileData={profileData} /></div>
-                <div><PersonalDetails {...data}></PersonalDetails></div>
-              </>
-  );
+        <div className={"pb-4"}><ProfileImageCard profileData={profileData} /></div>
+        <div><PersonalDetails {...data}></PersonalDetails></div>
+      </>
+
+
+  )
+      ;
 };
 
 export default Profile;
