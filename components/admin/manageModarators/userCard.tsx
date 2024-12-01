424b87e950126a16ad5ae216f25bb727184031b3
Sajith-Bandara <sajithbandara091@gmail.com>
2024-12-01 11:38:49 +0530

    edite moderator card

--- components/admin/manageModarators/userCard.tsx
+++ components/admin/manageModarators/userCard.tsx
@@ -1,20 +1,27 @@
 import React from "react";
 import { Card, Chip, Image, Button } from "@nextui-org/react";
 import { BiEdit, BiMessageRounded } from "react-icons/bi";
-import { RiDeleteBin5Line } from "react-icons/ri";
 import { RiEdit2Line } from "react-icons/ri";
 
 declare type ModeratorCard = {
-    name: string;
-    status: string;
-    imageUrl: string;
-    email: string;
+    id:string ,
+    email:string ,
+    role:string,
+    isActive: true,
+    createdAt: string,
+    firstName: string,
+    lastName: string,
+    city: string,
+    gender: string,
+    profilePic: string,
+    moderatorId:string
 };
 
 declare interface UserCardProps {
     user: ModeratorCard;
     onEdit: () => void;
-    onDelete: () => void;
+    onDelete: (id:string) => void;
+    onActive: (id:string) => void;
 }
 
 function emailLength(email: string) {
@@ -24,28 +31,31 @@ function emailLength(email: string) {
     return email.substring(0, 20) + '...';
 }
 
-export const UserCard = ({ user, onEdit, onDelete }: UserCardProps) => {
+export const UserCard = ({ user, onEdit, onDelete,onActive }: UserCardProps) => {
+
     return (
         <div className="relative">
             <Card className="min-w-[198.5px] p-2 relative">
                 <div
                     className=" background h-28 bg-gray-300 rounded-t-xl p-2 "
-                    style={{
-                        backgroundImage: `url(${user.imageUrl})`,
-                        backgroundSize: 'cover',
-                        backgroundPosition: 'center',
-                        filter: 'blur(8px)', // Apply blur effect here
-                        WebkitFilter: 'blur(8px)', // For Safari
-                }}>
+                    // style={{
+                    //     backgroundImage: `url(${user.profilePic})`,
+                    //     backgroundSize: 'cover',
+                    //     backgroundPosition: 'center',
+                    //     filter: 'blur(8px)', // Apply blur effect here
+                    //     WebkitFilter: 'blur(8px)', // For Safari
+                    // }}
+                >
 
-                    <div className=" flex justify-end mb-4">
+                    <div className=" flex justify-end mb-4 cursor-pointer">
                         <Chip
                             size="sm"
                             className={`${
-                                user.status === 'Active' ? 'bg-success-100 text-success-700' : 'bg-warning-100 text-warning-700'
+                                user.isActive ? 'bg-success-100 text-success-700' : 'bg-warning-100 text-warning-700'
                             }`}
+
                         >
-                            {user.status}
+                            {user.isActive?"Active":"Disable"}
                         </Chip>
                     </div>
                 </div>
@@ -55,26 +65,32 @@ export const UserCard = ({ user, onEdit, onDelete }: UserCardProps) => {
                         alt="nextui logo"
                         style={{ width: 100, height: "auto", aspectRatio: 1 }}
                         radius="full"
-                        src={user.imageUrl}
+                        src={user.profilePic}
                     />
                 </div>
 
                 <div className="flex justify-center mt-10">
-                    <p className="font-bold text-lg">{user.name}</p>
+                    <p className="font-bold text-lg">{user.firstName+" "+user.lastName}</p>
                 </div>
                 <div className="flex justify-center">
                     <p className="text-sm text-tertiaryText font-bold">{emailLength(user.email)}</p>
                 </div>
                 <div className="flex justify-center gap-2 mt-4 mb-4">
-                    <Button className="bg-recruitBlue" isIconOnly variant="faded" aria-label="message">
-                        <BiMessageRounded color={"white"} size={25} />
-                    </Button>
-                    <Button className="bg-gray-600" isIconOnly variant="faded" aria-label="edit" onPress={onEdit}>
-                        {/*<BiEdit color={"white"} size={25} />*/}
-                        <RiEdit2Line color={"white"} size={25}/>                    </Button>
-                    <Button className="bg-[#F31260]" isIconOnly variant="faded" aria-label="delete" onPress={onDelete}>
-                        <RiDeleteBin5Line color={"white"} size={25} />
-                    </Button>
+                    {
+                        user.isActive ?
+                            <Button  onClick={()=>onDelete(user.id)} className="bg-[#F31260] text-white font-bold w-full" >
+                                Deactivate
+                            </Button>:
+                            <Button onClick={()=>onActive(user.id)} className="bg-recruitBlue text-white font-bold w-full" >
+                                Activate
+                            </Button>
+                    }
+                    {/*<Button className="bg-gray-600 w-1/2" isIconOnly variant="faded" aria-label="edit" onPress={onEdit}>*/}
+                    {/*    /!*<BiEdit color={"white"} size={25} />*!/*/}
+                    {/*    <RiEdit2Line color={"white"} size={25}/>                    </Button>*/}
+                    {/*<Button className="bg-[#F31260]" isIconOnly variant="faded" aria-label="delete" onPress={onDelete}>*/}
+                    {/*    <RiDeleteBin5Line color={"white"} size={25} />*/}
+                    {/*</Button>*/}
                 </div>
             </Card>
         </div>
