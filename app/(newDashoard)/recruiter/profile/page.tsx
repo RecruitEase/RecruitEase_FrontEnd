3fe399f1c82f526de8471a7898e2ac5c3570f9c9
chathura226 <chathuralakshan226@gmail.com>
2024-12-04 08:52:09 +0530

    recruiter profile added

--- app/(newDashoard)/recruiter/profile/page.tsx
+++ app/(newDashoard)/recruiter/profile/page.tsx
@@ -1,12 +1,70 @@
-import React from 'react';
+"use client"
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'next/navigation';
+import LoadingComponent from '@/components/LoadingComponent';
+import ErrorComponent from '@/components/ErrorComponent';
+import { RecruiterProp } from '@/types/users';
+import { JobProps } from '@/types';
+import RecruiterJobs from '@/components/recruiterProfile/RecruiterJobs';
+import { getJobsByRecruiterId, getRecruiter } from '@/lib/api';
+import {useSession} from "next-auth/react";
+import {useCandidate} from "@/lib/hooks/useCandidates";
 
-const RecruiterProfile = () => {
+const Page = () => {
+    const [jobs, setJobs] = useState<JobProps[]>([]);
+    const [recruiterDetails, setRecruiterDetails] = useState<RecruiterProp>();
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
+
+    const {data:session}=useSession();
+
+
+
+    const recruiterId: string = session?.user.roleDetails.recruiterId;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            setLoading(true);
+            setError(false);
+            try {
+                const [jobData, recruiterData] = await Promise.all([
+                    getJobsByRecruiterId(recruiterId),
+                    getRecruiter(recruiterId)
+                ]);
+                setJobs(jobData);
+                setRecruiterDetails(recruiterData);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setError(true);
+            } finally {
+                setLoading(false);
+            }
+        };
+
+        if (recruiterId) {
+            fetchData();
+        }
+    }, [recruiterId]);
+
     return (
-        <div>
-            Recruiter Profile Settings
-            {/*todo*/}
-        </div>
+        (loading) ? <LoadingComponent />:
+            (error) ? <ErrorComponent />:
+                <>
+                    <div className="min-h-screen flex flex-col xl:flex-row xl:justify-center">
+                        {/* Main Section */}
+                        <div className="bg-white flex flex-col w-full xl:w-2/3">
+                            {recruiterDetails && (
+                                <RecruiterJobs
+                                    jobs={jobs}
+                                    companyName={recruiterDetails.companyName}
+                                    companyImg={recruiterDetails.profilePic}
+                                />
+                            )}
+                        </div>
+                    </div>
+                </>
     );
 };
 
-export default RecruiterProfile;
+export default Page;
